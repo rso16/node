c5d26ebca8dafbbfb566ed009595ae289747878e
KinkyPeanut69 <balintharmse24@gmail.com>
2018-12-21 11:42:17 +0100

    added nodeListList functionality

---
+++ node.c
@@ -0,0 +1,220 @@
+#include "node.h" 
+
+node *createNode(uint8_t data)
+{
+    node *newNode = malloc(sizeof(node));
+    newNode->data = data;
+    return newNode;
+}
+
+void setNextNode(node *currentNode, node *nextNode)
+{
+    currentNode->next = nextNode;
+}
+
+void addHeadToList(nodeList *list, node* newHead)
+{
+    list->head = newHead;
+}
+
+void removeHeadFromList(nodeList *list)
+{
+    //check if head exist
+    if(list->head != NULL)
+    {
+        //check if there is a next node
+        if(list->head->next != NULL)
+        {
+            node *newHead = list->head->next;
+            free(list->head);
+            list->head = newHead;
+        }
+        else
+        {
+            free(list->head);
+            list->head = NULL;
+        }
+	list->size--;
+    }
+    else
+    {
+        #if PRINTF == PRINTF_EN
+            printf("ERROR No head\n");
+        #endif
+        return;
+    }
+
+}
+
+void addNodeToList(nodeList *list, node *newNode)
+{
+    //check if head exist
+    if(list->head != NULL)
+    {
+        //check if tail exist
+        if(list->tail != NULL)
+        {   
+            node *temp = list->tail;
+            temp->next = newNode;
+            list->tail = newNode;
+        }
+        //tail doesn't exist
+        else
+        {
+            list->tail = newNode;
+            list->head->next = list->tail;
+        }
+	list->size++;
+    }
+    //head doesn't exist
+    else
+    {
+        addHeadToList(list, newNode);
+    }
+}
+
+void addNodeListToNodeListList(nodeListList *listList, nodeList *newNodeList)
+{
+        //check if head exist
+    if(listList->head != NULL)
+    {
+        //check if tail exist
+        if(listList->tail != NULL)
+        {   
+            nodeList *temp = listList->tail;
+            temp->head->next = newNodeList;
+            listList->tail = newNodeList;
+        }
+        //tail doesn't exist
+        else
+        {
+            listList->tail = newNodeList;
+            listList->head->head->next = listList->tail;
+        }
+	listList->size++;
+    }
+    //head doesn't exist
+    else
+    {
+        addHeadToList(listList, newNodeList);
+    }
+}
+
+void addDataToList(nodeList *list, uint8_t data)
+{
+   node *newNode = createNode(data);
+   addNodeToList(list, newNode);
+}
+
+void addStrToList(nodeList *list, char *str)
+{
+	int index = 0;
+	while(str[index] != '\0')
+	{
+		addDataToList(list, str[index]);
+		index++;
+	}	
+}
+
+void printNode(node *currentNode)
+{
+    
+   #if PRINTF == PRINTF_EN 
+        if(currentNode != NULL)
+        {
+            printf("data = %d\n", currentNode->data);
+        }
+        else
+        {
+            printf("ERROR is NULL\n");
+        }
+   #endif
+}
+
+void printList(nodeList *list)
+{
+   #if PRINTF == PRINTF_EN 
+        if(list->head != NULL)
+        {
+            node *currentNode = list->head;
+            int counter = 0;
+            while(currentNode != NULL)
+            {
+                printf("node %d = %d\n", counter, currentNode->data);
+                counter++;
+                currentNode = currentNode->next;
+            }
+        }
+        else
+        {
+            printf("error no list\n ");
+        }
+   #endif
+}
+
+void printStrList(nodeList *list)
+{
+   #if PRINTF == PRINTF_EN 
+        if(list->head != NULL)
+        {
+            node *currentNode = list->head;
+            int counter = 0;
+            while(currentNode != NULL)
+            {
+                printf("%c", currentNode->data);
+                counter++;
+                currentNode = currentNode->next;
+            }
+            printf("\n");
+        }
+        else
+        {
+            printf("error no list\n ");
+        }
+   #endif
+}
+
+
+int listToStr(nodeList *list, char *str, int begin, int end)
+{
+	//check params
+	//check if head exist
+	if(list->head == NULL)
+	{
+ 		return -1;
+	}
+	
+	//check if end is after begin
+	if(end <= begin)
+	{
+		return -2;
+	}
+	
+	//check boundries
+	if(end > list->size)
+	{
+		return -3;
+	}
+	
+	//make temp node
+	node *temp = list->head;		
+	//go to begin location
+	int index = 0;
+	while(index < begin)
+	{
+		index++;
+		temp = temp->next;
+	}
+		
+	//prepare string for concatenation
+	strcpy(str, "\0");
+	
+	while(index < end)
+	{	
+		strcat(str, &temp->data);
+		temp = temp->next;
+		index++;
+	}
+
+	return 1;
+}
---
+++ node.h
@@ -0,0 +1,52 @@
+//test idi nahui 
+#ifndef NODE_H
+#define NODE_H
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PRINTF_DI 0
+#define PRINTF_EN 1
+#define PRINTF PRINTF_EN
+
+typedef struct Node
+{
+	uint8_t data;
+    struct Node *next;
+}node;
+
+typedef struct LongNodeList
+{
+	struct Node *head;
+	struct Node *tail;
+	long   size;
+}longNodeList;
+
+typedef struct NodeList
+{
+	struct Node *head;
+	struct Node *tail;
+	uint8_t   size;
+}nodeList;
+
+typedef struct NodeListList
+{
+	struct NodeList *head;
+	struct NodeList *tail;
+	uint8_t	size;
+}nodeListList;
+
+node * createNode(uint8_t data);
+void setNextNode(node *currentNode, node *nextNode);
+void addHeadToList(nodeList *list, node* newHead);
+void removeHeadFromList(nodeList *list);
+void addNodeToList(nodeList *list, node *newNode);
+void addDataToList(nodeList *list, uint8_t data);
+void addNodeListToNodeListList(nodeListList * list, nodeList *nextNodeList);
+void addStrToList(nodeList *list, char *str); 
+void printNode(node *currentNode);
+void printList(nodeList *list);
+void printStrList(nodeList *list);
+int listToStr(nodeList *list, char *str, int begin, int end);
+#endif
